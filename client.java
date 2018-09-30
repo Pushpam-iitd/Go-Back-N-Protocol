fa91d291c378ba6d74808777dbc5f0bd5c1c5086
Nikhil Dilip Kulkarni <nikhil11kulkarni@gmail.com>
2018-09-30 21:31:51 +0530

    server - client is chattong successfully

--- client.java
+++ client.java
@@ -1,9 +1,26 @@
 import java.net.*;
+import java.util.*;
+import java.io.PrintStream;
 class client{
 	public static void main(String args[]){
 		try{
 				Socket s=new Socket("127.0.0.1",3000);
-				System.out.println("Conneted C Succesfully!");
+				System.out.println("Conneted client Succesfully!");
+				Scanner sc=new Scanner(System.in);
+				Scanner sc1=new Scanner(s.getInputStream());
+				PrintStream p=new PrintStream(s.getOutputStream());
+
+				int num1, number; 
+				while(true){
+					number=sc1.nextInt();
+					System.out.println("\nnumber received is: "+number);
+					System.out.print("Enter a number client: ");
+					num1=sc.nextInt();
+					p.println(num1);
+
+				}
+
+
 
 		}catch(Exception e){
 
--- server.java
+++ server.java
@@ -1,10 +1,25 @@
 import java.net.*;
+import java.util.*;
+import java.io.PrintStream;
 class server{
 	public static void main(String args[]){
 		try{
 				ServerSocket server =new ServerSocket(3000);
 				Socket s=server.accept();
 				System.out.println("Conneted Succesfully!");
+				Scanner sc=new Scanner(System.in);
+				Scanner sc1=new Scanner(s.getInputStream());
+				PrintStream p=new PrintStream(s.getOutputStream());
+
+				int num1, number; 
+				while(true){
+					System.out.print("Enter a number server: ");
+					num1=sc.nextInt();
+					p.println(num1);
+					number=sc1.nextInt();
+					System.out.println("\nnumber received is: "+number);
+
+				}
 
 		}catch(Exception e){
 
